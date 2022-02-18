39de195d4d2e15aa655181ed95eb7922b3c66427
Adriel Coria <99737740+Adriel3002@users.noreply.github.com>
2022-02-18 11:25:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 
 let suma = num1 => num1 * 2;
 console.log(suma(6))
+
+let fruta = 'Manzana'
+let resultado = fruta == 'Manzana' ? 'Perfecto, me gusta la Manzana' : 'Que lastima tenia Hambre';
+console.log (resultado);
